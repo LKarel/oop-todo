6da41469c24c0d844f3b93c2c451c34edcf96115
Ragnis Armus <ragnis.armus@gmail.com>
2015-05-01 15:06:20 +0300

    Clean up TasksWriter

--- src/main/java/todoapp/TasksWriter.java
+++ src/main/java/todoapp/TasksWriter.java
@@ -22,7 +22,6 @@ public class TasksWriter
 	 */
 	public void write(TaskList tasks)
 	{
-
 		String filePath = file.getAbsolutePath();
 
 		file.delete();
@@ -32,17 +31,11 @@ public class TasksWriter
 		{
 			PrintWriter printWriter = new PrintWriter(file);
 
-			for (Task task : tasks)
-			{
-				printWriter.println(taskToCSV(task));
-			}
-
 			printWriter.close();
 		}
 		catch (FileNotFoundException e)
 		{
 			System.out.println("Invalid file");
-			return;
 		}
 	}
 
