ffe003b9bbe5f51821bdb5b0a2c75f36232af927
Ragnis Armus <ragnis.armus@gmail.com>
2015-05-01 14:53:32 +0300

    Create TaskPane elements

--- src/main/java/todoapp/gui/TaskPane.java
+++ src/main/java/todoapp/gui/TaskPane.java
@@ -1,20 +1,45 @@
 package todoapp.gui;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Pos;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.HBox;
 
 import todoapp.Task;
 
-public class TaskPane extends VBox
+public class TaskPane extends HBox
 {
 	private Task task;
 
 	public TaskPane(Task task)
 	{
-		super(10);
+		super(5);
 		this.task = task;
 
-		getChildren().add(new Label(task.toString()));
+		CheckBox mark = new CheckBox();
+		mark.setSelected(task.getDone());
+
+		mark.selectedProperty().addListener(new ChangeListener<Boolean>()
+		{
+			public void changed(ObservableValue<? extends Boolean> ov,
+				Boolean oldValue, Boolean newValue)
+			{
+				task.setDone(newValue);
+			}
+		});
+
+		Label deadline = new Label(task.getDeadline().toString());
+		deadline.setPrefWidth(120);
+		deadline.setMinWidth(120);
+
+		Label text = new Label(task.getName());
+
+		getChildren().add(mark);
+		getChildren().add(deadline);
+		getChildren().add(text);
 	}
 }
