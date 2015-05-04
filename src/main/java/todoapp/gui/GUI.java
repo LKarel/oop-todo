5f3494a9e94e4f9105ddc9e4329659c4c55009d8
Ragnis Armus <ragnis.armus@gmail.com>
2015-05-04 19:02:32 +0300

    Implement adding tasks

--- src/main/java/todoapp/gui/GUI.java
+++ src/main/java/todoapp/gui/GUI.java
@@ -1,16 +1,28 @@
 package todoapp.gui;
 
 import java.io.File;
+import java.time.LocalDateTime;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import todoapp.DateParser;
+import todoapp.Task;
 import todoapp.TaskList;
 import todoapp.TasksReader;
 import todoapp.TasksWriter;
@@ -34,7 +46,70 @@ public class GUI extends Application
 		primaryStage.setTitle("Todo app");
 		primaryStage.setMinWidth(300);
 
-		StackPane root = new StackPane();
+		VBox root = new VBox();
+
+		HBox inputBar = new HBox(5);
+		inputBar.setPadding(new Insets(10));
+
+		boolean dateOk = false;
+
+		TextField dateField = new TextField();
+		dateField.setPrefWidth(110);
+		dateField.setMinWidth(TextField.USE_PREF_SIZE);
+
+		TextField nameField = new TextField();
+		nameField.setMaxWidth(Double.MAX_VALUE);
+		HBox.setHgrow(nameField, Priority.ALWAYS);
+
+		dateField.textProperty().addListener(new ChangeListener<String>()
+		{
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
+			{
+				dateField.getStyleClass().removeAll("textField-error");
+
+				if (newValue.length() > 0 && DateParser.parse(newValue) == null)
+				{
+					dateField.getStyleClass().add("textField-error");
+				}
+			}
+		});
+
+		dateField.setOnKeyPressed(new EventHandler<KeyEvent>()
+		{
+			@Override
+			public void handle(KeyEvent key)
+			{
+				if (key.getCode().equals(KeyCode.ENTER))
+				{
+					nameField.requestFocus();
+				}
+			}
+		});
+
+		nameField.setOnKeyPressed(new EventHandler<KeyEvent>()
+		{
+			@Override
+			public void handle(KeyEvent key)
+			{
+				if (key.getCode().equals(KeyCode.ENTER))
+				{
+					LocalDateTime deadline = DateParser.parse(dateField.getText());
+
+					if (deadline != null)
+					{
+						tasks.add(new Task(nameField.getText(), deadline));
+
+						dateField.getStyleClass().removeAll("textField-error");
+						dateField.setText("");
+						nameField.setText("");
+					}
+				}
+			}
+		});
+
+		inputBar.getChildren().add(dateField);
+		inputBar.getChildren().add(nameField);
 
 		ScrollPane scroll = new ScrollPane();
 		scroll.setContent(new TasksPane(tasks));
@@ -42,9 +117,14 @@ public class GUI extends Application
 		scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 		scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
 
+		root.getChildren().add(inputBar);
+		root.getChildren().add(new Separator());
 		root.getChildren().add(scroll);
 
-		primaryStage.setScene(new Scene(root, 300, 250));
+		Scene scene = new Scene(root, 300, 250);
+		scene.getStylesheets().add("style.css");
+
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 
